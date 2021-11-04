d632bd99f5d3f2039330f5b5b2c9d8cd13cf5851
Artemcs95 <43315150+Artemcs95@users.noreply.github.com>
2021-11-04 19:23:49 +0200

    Delete Task 1.py

--- Task 1.py
+++
@@ -1,2 +0,0 @@
-import random
-print (random.choice(open("Wordlist1.txt").readline().split()),random.choice(open("Wordlist2.txt").readline().split()),random.choice(open("Wordlist3.txt").readline().split()))
